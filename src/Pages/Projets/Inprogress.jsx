b6c0f284e3353eeea40fb0c537b961cba354d16f
Delphine Sa|mon Barrabes <delphinebarrabessalmon@gmail.com>
2025-05-20 11:02:47 +0200

    create new json and page inprogress

--- src/Pages/Projets/Inprogress.jsx
+++ src/Pages/Projets/Inprogress.jsx
@@ -1,18 +1,34 @@
 import { Link } from 'react-router-dom'
+import Card from '../../Components/Card/Card'
+import CurrentProject from '../../Data/CurrentProject.json'
+import'./Inprogress.scss'
 
 function InProgress() {
     return (
-        <div className="accueil">
+        <div className="inprogress-page">
             <div className="title-animation">
                 
                 <h1 className="title">  Mes Projets en cours 🧶</h1>
+                <h2 className="second-title">Des mailles en devenir...</h2>
+                <p className='text-inprogress'>Cet espace me permet de suivre leur évolution, noter mes idées, conserver les détails essentiels : type de laine, taille d’aiguilles, points utilisés… Un carnet vivant qui m’aide à garder le fil sans perdre la magie.
+
+Entre organisation et création, cette page est mon fil conducteur dans l’univers infini du tricot..</p>
                
 <Link className="boutonprojet" to="/Projet">
                     Ajouter un projet 
                 </Link>
+
+                <div className="containercards">
+                    <div className="cards">
+                        {CurrentProject.map((projet) => (
+                            <Card key={projet.id} projet={projet} />
+                        ))}
+                    </div>
+                </div>
                 <div className="btn-group my-5"></div>
             </div>
         </div>
+        
     )
 }
 
--- src/Pages/Projets/Knitlist.jsx
+++ src/Pages/Projets/Knitlist.jsx
@@ -6,10 +6,16 @@ import'./knitlist.scss'
 
 function KnitlistPage() {
     return (
-        <div className="accueil">
+        <div className="knit-list-page">
             <div className="title-animation">
                 
-                <h1 className="title">  Knit-List 🧶</h1>
+                <h1 className="title">  🧶 Knit-List, </h1>
+                <h2 className="second-title">Le fil de tes envies</h2>
+                <p className='text-kintlist'>Dans cet espace, chaque idée a sa place : un gilet repéré au détour d’un fil Instagram, un bonnet doux à offrir, une couverture que l’on rêve de monter maille après maille. Ici, tu peux enregistrer tes projets futurs, noter les fils à utiliser, et garder le fil de tes inspirations. Une liste qui t’accompagne au rythme de tes aiguilles.</p>
+
+                <Link className="boutonprojet" to="/Projet">
+                    Ajouter une envie 
+                </Link>
 
                 <div className="containercards">
                     <div className="cards">
@@ -19,9 +25,7 @@ function KnitlistPage() {
                     </div>
                 </div>
                
-<Link className="boutonprojet" to="/Projet">
-                    Ajouter une envie 
-                </Link>
+
                 <div className="btn-group my-5"></div>
             </div>
         </div>
